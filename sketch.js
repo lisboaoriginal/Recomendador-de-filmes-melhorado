c31983171a5dbe7de27676ea7e9231ac9a9d6b41
lisboaoriginal <joao.martins.oliveira22@escola.pr.gov.br>
2025-05-27 11:21:52 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,41 @@
+function geraRecomendacao(idade, gostaDeFantasia) {
+    if (idade >= 10) {
+        if (idade >= 14) {
+            return "O menino que descobriu o vento";
+        } else {
+            if (gostaDeFantasia) {
+                return "As aventuras de pi";
+            } else {
+                return "Depois da chuva";
+            }
+        }
+    } else {
+        if (gostaDeFantasia) {
+            return "A viagem de chihiro";
+        } else {
+            return "O feitiço do tempo";
+        }
+    }
+}
+function setup() {
+    createCanvas(800, 400);
+
+    // 🔵 Texto para informar ao usuário
+    createSpan("Sua idade: ");
+    campoIdade = createInput("5");
+
+    // 🔵 Checkbox com descrição clara
+    campoFantasia = createCheckbox(" Gosta de fantasia?");
+}
+function draw() {
+    background("yellow"); // ⚪ Fundo branco
+    let idade = campoIdade.value();
+    let gostaDeFantasia = campoFantasia.checked();
+    let recomendacao = geraRecomendacao(idade, gostaDeFantasia);
+
+    fill(color("blue")); // 🟣 Cor do texto (roxo escuro)
+    textAlign(CENTER, CENTER); // 🎯 Alinhamento centralizado
+    textSize(38); // 🔠 Tamanho maior para boa leitura
+
+    text(recomendacao, width / 2, height / 2); // 📍 Texto exibido no centro
+}
